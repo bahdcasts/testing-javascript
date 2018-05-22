d950b658cfe1f65289c72cf66048926c252c2c8d
Kati Frantz <frantzenterprises777@gmail.com>
2018-05-22 02:51:17 +0100

    aserting that function throws error

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function add (num1, num2) {
+  if (!num1 || !num2) {
+    throw new Error('Numbers are required for adding.')
+  }
   return num1 + num2;
 }
 
--- index.spec.js
+++ index.spec.js
@@ -1,8 +1,23 @@
 const assert = require('assert')
 const { add } = require('./index')
 
-const actual = add(5, 6)
-const expectation = 11
 
-assert.equal(actual, expectation)
-console.log('Successfully ran all tests !')
+console.log('First test')
+  const actual = add(5, 6)
+  const expectation = 11
+
+  assert.equal(actual, expectation)
+  console.log('Successfully ran all tests !')
+
+console.log('End of First test')
+
+console.log('Second test: Test that add function throws an error if no arguments are passed.')
+  
+  assert.throws(() => {
+    add()
+  })
+
+  console.log('Successfully ran all tests !')
+
+console.log('End of Second test')
+
