a1cbdad9f2bef1fc5a575e6cd5efbd9f1af70a0b
Kati Frantz <frantzenterprises777@gmail.com>
2018-05-22 03:42:14 +0100

    add more test cases for the parse function

--- src/test/query.spec.js
+++ src/test/query.spec.js
@@ -14,11 +14,24 @@ describe('The query package', function () {
 
       assert.deepEqual(actual, expectation)
     })
+
+    it('Should return an object: { number: 3 } when ?number=3 is passed into it', function () {
+      assert.deepEqual(parse('?number=3'), {
+        number: 3
+      })
+    })
   })
 
   describe('The stringify function', function () {
     it('Should return a query string when an object is passed into it', () => {
-      // tests go here.
+      const queryObject = {
+        by: 'kati-frantz',
+        sort: 'popular'
+      }
+
+      const actual = stringify(queryObject)
+      const expectation = 'by=kati-frantz&sort=popular'
+      assert.equal(actual, expectation)
     })
   })
 })
